7e881ee9115c1d770824a0895ac963ddb542b000
waynelee-lwc <1418551940@qq.com>
2022-06-17 22:49:34 +0800

    [bugfix]rem size

--- static/student.js
+++ static/student.js
@@ -1,4 +1,8 @@
-
+document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
+window.onresize = function(){
+	// console.log("当前尺寸为：" + window.innerWidth);
+	document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
+}
 
 $(document).ready(()=>{
     

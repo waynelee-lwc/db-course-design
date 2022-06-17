2b71db84199b5238abec9bcd43423e43bc5084fd
waynelee-lwc <1418551940@qq.com>
2022-06-17 21:30:14 +0800

    [feat]登录框提交

--- static/index.js
+++ static/index.js
@@ -4,3 +4,11 @@ $('.role-radio').click(function(e){
     console.log(e)
     console.log($(e).attr('id'))
 })
+
+$('.login-submit').on('click',function(){
+    let userid = $('.ID').val()
+    let password = $('.password').val()
+    let role = $("input:radio:checked").val()
+
+    alert(userid + password + role)
+})

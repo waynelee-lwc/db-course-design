69878dc6ce563dcbf7efde15d6c27b19b318dbfd
waynelee-lwc <1418551940@qq.com>
2022-06-17 20:24:19 +0800

    [new]init project

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var multiparty = require('connect-multiparty')
+
+let app = express()
+app.use('/',express.static('./static'))
+
+//处理 x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+//处理 application/json
+app.use(bodyParser.json())
+
+//处理 mutipart/form-data
+app.use(multiparty())
+
+app.get('/hello',(req,res)=>{
+    res.end('hello world')
+})
+
+let server = app.listen(3011,()=>{
+    console.log('The server is listening on port : 3011')
+})

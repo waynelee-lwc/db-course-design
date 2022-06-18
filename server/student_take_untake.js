bf0d3ec8c143f4ccab97e7646f05bb6109f8a19e
Superbia-zyb <1139084122@qq.com>
2022-06-18 18:29:43 +0800

    zyb

--- server/student_take_untake.js
+++ server/student_take_untake.js
@@ -29,12 +29,50 @@ async function search_student_section_list_takable(req, res) {
     	return 
     }
 
-    sql = mysql.format("select count(*) as count from section join takes on ")
-   	res.send({
-   		"message":"",
-   		"code":200,
-   		"data": result
-   	})
+    sql = mysql.format("select count(*) as count from section join takes on takes.sec_id = section.sec_id where section.sec_id = ? and takes.SID = ? and section.status = 2 ", [data.sec_id, raw_data.id])
+   	result = await query(sql)
+   	    if(result.length == 0) {
+    	res.send({
+    		"message": "查询 error",
+    		"code": 400
+    	})
+    	return 
+    }
+
+    var count = result[0].count
+    if(data.operation == 'take') {
+    	if(count > 0) {
+    		res.send({
+    			"message": "你已经选了该课程",
+    			"code": 400
+    		})
+    		return
+    	}
+    	sql = mysql.format("insert into takes(SID, sec_id, grade) VALUES (?,?,?)", [raw_data.id, data.sec_id, ""])
+    	result = await query(sql)
+    	res.send({
+    		"message": "",
+    		"code": 200
+    	})
+    	return 
+    }
+    
+    if(data.operation == 'untake') {
+    	if(count == 0) {
+     		res.send({
+    			"message": "你没有选该课程",
+    			"code": 400
+    		})
+    		return   		
+    	}
+    	sql = mysql.format("delete from takes where takes.SID = ? and takes.sec_id = ?", [raw_data.id, data.sec_id])
+		result = await query(sql)
+    	res.send({
+    		"message": "",
+    		"code": 200
+    	})
+    	return 
+    }
    	return 
 }
 module.exports = search_student_section_list_takable

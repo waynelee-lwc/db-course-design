c266f8ec6f86a4c495252ad34ce30688be05f896
waynelee-lwc <1418551940@qq.com>
2022-06-18 19:01:06 +0800

    [feat]学生选课

--- static/student.js
+++ static/student.js
@@ -32,6 +32,9 @@ $(document).ready(()=>{
 
     $('.phone input').val(user.phone)
     $('.email input').val(user.email)
+
+    reloadTakable()
+    hideShadow()
     
 })
 
@@ -72,6 +75,7 @@ $('.profile-submit').on('click',function(){
 })
 
 $('.cross').click(hideShadow)
+$('.search-submit').click(reloadTakable)
 
 function hideShadow(){
     $('.shadow').hide()
@@ -79,4 +83,80 @@ function hideShadow(){
 
 function showShadow(){
     $('.shadow').show()
+}
+
+function reloadTakable(){
+    
+    let course_name = $('.course_name input').val()
+    let dept_name = $('.dept_name input').val()
+    let teacher_name = $('.teacher_name input').val()
+    let semester = $('.semester input').val()
+
+    //学生可选课程列表
+    $.ajax({
+        url:`${address}/student/section_list_takable`,
+        type:'get',
+        headers:{
+            // token:JSON.parse(localStorage.getItem('token'))
+            token:'student'
+        },
+        data:{
+            course_name:course_name,
+            teacher_name:teacher_name,
+            dept_name:dept_name,
+            year:'',
+            semester:''  
+        },
+        success:function(res){
+            console.log(res)
+            if(res.code != 200){
+                alert('表单加载错误！')
+            }else{
+                $('.section-table tbody').empty()
+                for(let sec of res.data){
+                    $('.section-table tbody').append(
+                        $(`
+                        <tr>
+                            <td>${sec.course_id}</td>
+                            <td>${sec.sec_id}</td>
+                            <td>${sec.title}</td>
+                            <td>${sec.type}</td>
+                            <td>${sec.dept_name}</td>
+                            <td>${sec.credits}</td>
+                            <td>${sec.year} ${sec.semester}</td>
+                            <td>${sec.teacher_names}</td>
+                            <td><button class="btn btn-primary" id="schedule-${sec.sec_id}">查看</button></td>
+                            <td><button class="btn btn-${sec.SID ? 'success' : 'danger'} take-untake" id="${sec.SID ? 'untake' : 'take'}-${sec.sec_id}">${sec.SID ? '退选' : '选课'}</button></td>
+                        </tr>
+                        `)
+                    )
+                }
+            }
+            $('.take-untake').click(function(){
+                id = $(this).attr('id')
+                operation = id.split('-')[0]
+                sec_id = id.split('-')[1]
+                $.ajax({
+                    url:`${address}/student/take_untake`,
+                    type:'post',
+                    headers:{
+                        // token:JSON.parse(localStorage.getItem('token'))
+                        token:'student'
+                    },
+                    data:{
+                        operation:operation,
+                        sec_id:sec_id
+                    },
+                    success:function(res){
+                        if(res.code != 200){
+                            alert(res.message)
+                        }else{
+                            alert(operation == 'take' ? '选课成功!' : '退选成功!')
+                            reloadTakable()
+                        }
+                    }
+                })
+            })
+        }
+    })
 }

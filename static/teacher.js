d7a7030e69df7c8956e7330ca023d4260eac4a8c
waynelee-lwc <1418551940@qq.com>
2022-06-18 13:52:54 +0800

    [feat]教师和学生页面更换背景

--- static/teacher.js
+++ static/teacher.js
@@ -3,15 +3,16 @@ window.onresize = function(){
     // console.log("当前尺寸为：" + window.innerWidth);
     document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
 }
-
+let address = 'http://60.205.211.19:3011'
 $(document).ready(()=>{
 
-    let user ={
-        name:'大强',
-        dept_name:'计算机',
-        email:'123456789@jlu.edu.cn',
-        phone:'12345678901'
-    }
+    // let user ={
+    //     name:'大强',
+    //     dept_name:'计算机',
+    //     email:'123456789@jlu.edu.cn',
+    //     phone:'12345678901'
+    // }
+    let user = JSON.parse(localStorage.getItem('userinfo'))
 
     $('.userinfo').empty()
     $('.userinfo').html(
@@ -25,13 +26,38 @@ $(document).ready(()=>{
 
 $('.profile-submit').on('click',function(){
 
-    let id = '3123123123'
+    let user = JSON.parse(localStorage.getItem('userinfo'))
     let phone = $('.phone input').val()
     let email = $('.email input').val()
     let password = $('.password input').val()
     let passwordrpt = $('.passwordrpt input').val()
 
-    alert('id ' + id + '\nphone ' + phone + '\nemail ' + email + '\npassword ' + password + '\npasswordrpt ' + passwordrpt)
+    if(password != passwordrpt){
+        alert('两次密码不一致!')
+        return
+    }
+    // alert('id ' + id + '\nphone ' + phone + '\nemail ' + email + '\npassword ' + password + '\npasswordrpt ' + passwordrpt) 
+    $.ajax({
+        url:`${address}/user/update_profile`,
+        headers:{
+            token:JSON.parse(localStorage.getItem('token'))
+        },
+        type:'post',
+        data:{
+            phone:phone,
+            email:email,
+            password:password
+        },
+        success:function(res){
+            if(res.code != 200){
+                alert(res.message)
+            }else{
+                localStorage.setItem('userinfo',JSON.stringify(res.data.user))
+                alert('修改成功!')
+                location.reload()
+            }
+        }
+    })
 })
 
 $('.search-submit').on('click',function (){

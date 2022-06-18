6f1b1fe21cb0c03bff9f13fccbdb239458a7803c
waynelee-lwc <1418551940@qq.com>
2022-06-18 23:37:13 +0800

    [feat]教室选课

--- static/teacher.js
+++ static/teacher.js
@@ -4,14 +4,9 @@ window.onresize = function(){
     document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
 }
 let address = 'http://60.205.211.19:3011'
-$(document).ready(()=>{
 
-    // let user ={
-    //     name:'大强',
-    //     dept_name:'计算机',
-    //     email:'123456789@jlu.edu.cn',
-    //     phone:'12345678901'
-    // }
+
+$(document).ready(()=>{
     let user = JSON.parse(localStorage.getItem('userinfo'))
 
     $('.userinfo').empty()
@@ -22,6 +17,10 @@ $(document).ready(()=>{
     $('.phone input').val(user.phone)
     $('.email input').val(user.email)
 
+    //选课列表
+    reloadTakable()
+    //历史任课
+    loadSchedule()
 })
 
 $('.profile-submit').on('click',function(){
@@ -40,7 +39,8 @@ $('.profile-submit').on('click',function(){
     $.ajax({
         url:`${address}/user/update_profile`,
         headers:{
-            token:JSON.parse(localStorage.getItem('token'))
+            // token:JSON.parse(localStorage.getItem('token'))
+            token:'teacher'
         },
         type:'post',
         data:{
@@ -60,6 +60,9 @@ $('.profile-submit').on('click',function(){
     })
 })
 
+$('.cross').click(hideShadow)
+$('.search-submit').click(reloadTakable)
+
 $('.search-submit').on('click',function (){
     let course_id=$('.course_id input').val();
     let dept_name=$('.dept_name input').val();
@@ -67,3 +70,176 @@ $('.search-submit').on('click',function (){
     let status=$('.status select').val();
     alert('cid ' + course_id + '\ndeptname ' + dept_name + '\nsemester ' + semester + '\nstatus ' + status)
 })
+
+
+function hideShadow(){
+    $('.shadow').hide()
+}
+
+function showShadow(){
+    $('.shadow').show()
+}
+
+let historyTakes = []
+function loadSchedule(){
+    $.ajax({
+        url:`${address}/teacher/take_list`,
+        type:'get',
+        headers:{
+            // token:JSON.parse(localStorage.getItem('token'))
+            token:'teacher'
+        },
+        success:function(res){
+            historyTakes = res.data
+        }
+    })
+}
+
+let takable = []
+function reloadTakable(){
+    
+    // let course_name = $('.course_name input').val()
+    // let dept_name = $('.dept_name input').val()
+    // let teacher_name = $('.teacher_name input').val()
+    // let semester = $('.semester select').val()
+    // let year = semester.split('-')[0]
+    // semester = semester.split('-')[1]
+
+    //学生可选课程列表
+    $.ajax({
+        url:`${address}/teacher/section_list_takable`,
+        type:'get',
+        headers:{
+            // token:JSON.parse(localStorage.getItem('token'))
+            token:'teacher'
+        },
+        data:{
+            sec_id:'',
+            course_name:'',
+            dept_name:'',
+            year:'',
+            semester:''
+        },
+        success:function(res){
+            console.log(res)
+            if(res.code != 200){
+                alert('表单加载错误！')
+            }else{
+                takable = res.data
+                $('.section-table tbody').empty()
+                for(let idx in res.data){
+                    sec = res.data[idx]
+                    $('.section-table tbody').append(
+                        $(`
+                        <tr>
+                            <td>${sec.course_id}</td>
+                            <td>${sec.sec_id}</td>
+                            <td>${sec.title}</td>
+                            <td>${sec.course_type}</td>
+                            <td>${sec.dept_name}</td>
+                            <td>${sec.credits}</td>
+                            <td>${sec.year} ${sec.semester}</td>
+                            <td><button class="btn btn-primary check-schedule" id="${idx}-${sec.sec_id}">查看</button></td>
+                            <td><button class="btn btn-${sec.IID ? 'success' : 'danger'} take-untake" id="${sec.IID ? 'untake' : 'take'}-${sec.sec_id}">${sec.IID ? '退选' : '选课'}</button></td>
+                        </tr>
+                        `)
+                    )
+                }
+            }
+            $('.take-untake').click(takeUntake)
+            $('.check-schedule').click(checkSchedule)
+        }
+    })
+}
+
+function takeUntake(){
+    id = $(this).attr('id')
+    operation = id.split('-')[0]
+    sec_id = id.split('-')[1]
+    $.ajax({
+        url:`${address}/teacher/take_untake`,
+        type:'post',
+        headers:{
+            // token:JSON.parse(localStorage.getItem('token'))
+            token:'teacher'
+        },
+        data:{
+            operation:operation,
+            sec_id:sec_id
+        },
+        success:function(res){
+            if(res.code != 200){
+                alert(res.message)
+            }else{
+                reloadTakable()
+                loadSchedule()
+                alert(operation == 'take' ? '选择授课成功!' : '退选成功!')
+            }
+        }
+    })
+}
+
+function checkSchedule(){
+    let id = $(this).attr('id')
+    let sec_id = id.split('-')[1]
+    let idx = id.split('-')[0]
+    sec = takable[idx]
+    console.log(sec)
+
+    $.ajax({
+        url:`${address}/search/get_time_slog_by_sec_id`,
+        type:'get',
+        data:{
+            sec_id:sec_id
+        },
+        success:function(res){
+            timeslot = res.data.timeSlot
+            table = parseSchedule(timeslot)
+            setCurrTable(table)
+            initHistoryTable(table)
+            refreshView()
+            showShadow()
+
+            $('.schedule-title').empty()
+            $('.schedule-title').append($(`
+                <div class="name">${sec.title}</div>
+                <div class="teachers">${sec.teacher_names}</div>
+                <div class="type">${sec.course_type}</div>
+                <div class="dept">${sec.dept_name}</div>
+                <div class="semester">${sec.year} ${sec.semester}</div>
+            `))
+
+            $('.schedule-panel').empty()
+            $('.schedule-panel').append($(`
+                <div class="begin-week">从第<b>${timeslot.begin_week}</b>周开始</div>
+                <div class="end-week">到第<b>${timeslot.end_week}</b>周结束</div>
+                <div class="building">教学楼:<b>${sec.building}</b></div>
+                <div class="classroom">教室:<b>${sec.room_number}</b></div>
+                <button class="btn btn-primary show-history">展示已选课程</button>
+            `))
+            $('.show-history').click(loadHistory)
+        }
+    })
+}
+
+function loadHistory(){
+    let val = $(this).text()
+    if(val == '展示已选课程'){
+        initHistoryTable()
+        for(sec of historyTakes){
+            table = parseSchedule(sec)
+            for(let i = 1;i <= 7;i++){
+                for(let j = 1;j <= 11;j++){
+                    historyTable[i][j] += table[i][j]
+                }
+            }
+        }
+        refreshView()
+        $(this).text('隐藏已选课程')
+    }else{
+        initHistoryTable()
+        refreshView()
+        $(this).text('展示已选课程')
+    }
+    
+}

94dc986ad529c3c3f663131033f9bad784ea24ac
waynelee-lwc <1418551940@qq.com>
2022-06-18 13:33:11 +0800

    [feat]调通了登录

--- static/index.js
+++ static/index.js
@@ -4,6 +4,8 @@ window.onresize = function(){
 	document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
 }
 
+let address = 'http://60.205.211.19:3011'
+
 $('.role-radio').click(function(e){
     console.log(e)
     console.log($(e).attr('id'))
@@ -14,5 +16,30 @@ $('.login-submit').on('click',function(){
     let password = $('.password').val()
     let role = $("input:radio:checked").val()
 
-    alert(userid + password + role)
+    $.ajax({
+        url:`${address}/user/login`,
+        type:'post',
+        data:{
+            id:userid,
+            password:password,
+            role:role
+        },
+        success:function(res){
+            if(res.code != 200){
+                alert(res.message)
+            }else{
+                localStorage.setItem('userinfo',JSON.stringify(res.data.user))
+                localStorage.setItem('token',JSON.stringify(res.data.token))
+                if(role == 'student'){
+                    location.href = '/student.html'
+                }
+                if(role == 'teacher'){
+                    location.href = '/teacher.html'
+                }
+                if(role == 'admin'){
+                    location.href = '/admin.html'
+                }
+            }
+        }
+    })
 })
--- static/student.js
+++ static/student.js
@@ -4,14 +4,18 @@ window.onresize = function(){
 	document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
 }
 
+let address = 'http://60.205.211.19:3011'
+
 $(document).ready(()=>{
     
-    let user ={
-        name:'大明明',
-        dept_name:'环境工程',
-        email:'123456789@jlu.edu.cn',
-        phone:'12345678901'
-    }
+    // let user ={
+    //     name:'大明明',
+    //     dept_name:'环境工程',
+    //     email:'123456789@jlu.edu.cn',
+    //     phone:'12345678901'
+    // }
+    let user = JSON.parse(localStorage.getItem('userinfo'))
+
 
     $('.userinfo').empty()
     $('.userinfo').html(
@@ -25,11 +29,36 @@ $(document).ready(()=>{
 
 $('.profile-submit').on('click',function(){
 
-    let id = '3123123123'
+    let user = JSON.parse(localStorage.getItem('userinfo'))
     let phone = $('.phone input').val()
     let email = $('.email input').val()
     let password = $('.password input').val()
-    let passwordrpt = $('.password input').val()
+    let passwordrpt = $('.passwordrpt input').val()
 
-    alert('id ' + id + '\nphone ' + phone + '\nemail ' + email + '\npassword ' + password + '\npasswordrpt ' + passwordrpt) 
+    if(password != passwordrpt){
+        alert('两次密码不一致!')
+        return
+    }
+    // alert('id ' + id + '\nphone ' + phone + '\nemail ' + email + '\npassword ' + password + '\npasswordrpt ' + passwordrpt) 
+    $.ajax({
+        url:`${address}/user/update_profile`,
+        headers:{
+            token:JSON.parse(localStorage.getItem('token'))
+        },
+        type:'post',
+        data:{
+            phone:phone,
+            email:email,
+            password:password
+        },
+        success:function(res){
+            if(res.code != 200){
+                alert(res.message)
+            }else{
+                localStorage.setItem('userinfo',JSON.stringify(res.data.user))
+                alert('修改成功!')
+                location.reload()
+            }
+        }
+    })
 })
